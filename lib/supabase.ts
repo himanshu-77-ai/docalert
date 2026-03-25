d8b70ee91b91cc01f740e3d3381315a01047f84c
himanshu-77-ai <himansh.cs91@gmail.com>
2026-03-25 18:21:26 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase configuration
 const supabaseUrl = 'https://mwtnayogicrmnapubimv.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13dG5heW9naWNybW5hcHViaW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NzkwOTAsImV4cCI6MjA4OTI1NTA5MH0.bmYPWsfHbC7dSkKhWauEY4lS97S1Fo3VVflv115_dFc'
 
